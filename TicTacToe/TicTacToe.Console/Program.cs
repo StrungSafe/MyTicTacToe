a26f83d2fa06cf186860c4833f61721ae0beef1c
Miguel Molina Jr <mmolina@foldingcoin.net>
2018-08-07 23:35:26 -0400

    Initial add for console and windows forms app

---
+++ TicTacToe/TicTacToe.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TicTacToe.Console
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe.WindowsDesktop.Forms/Form1.cs
@@ -0,0 +1,12 @@
+﻿namespace TicTacToe.WindowsDesktop.Forms
+{
+    using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
