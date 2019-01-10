1bdde072acad91b419f4792146647c4bf6a166b1
Miguel Molina Jr <molim134@gmail.com>
2019-01-09 23:13:37 -0500

    Provide an initial state for the tic tac toe gameboard

--- TicTacToe/TicTacToe.ReactWebApp/ClientApp/src/store/TicTacToe.js
+++ TicTacToe/TicTacToe.ReactWebApp/ClientApp/src/store/TicTacToe.js
@@ -1,6 +1,15 @@
-﻿const initialState = { };
+﻿const initialState = { p1Btn_Value: '', p2Btn_Value: '', p3Btn_Value: '', p4Btn_Value: '', p5Btn_Value: '', p6Btn_Value: '', p7Btn_Value: '', p8Btn_Value: '', p9Btn_Value: '' };
 
 export const actionCreators = {
+    p1Btn_Click: () => ({}),
+    p2Btn_Click: () => ({}),
+    p3Btn_Click: () => ({}),
+    p4Btn_Click: () => ({}),
+    p5Btn_Click: () => ({}),
+    p6Btn_Click: () => ({}),
+    p7Btn_Click: () => ({}),
+    p8Btn_Click: () => ({}),
+    p9Btn_Click: () => ({})
 };
 
 export const reducer = (state, action) => {
