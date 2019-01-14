65a8ab4d1aa0c1a6970b73c250b793ff45a56554
Miguel Molina Jr <molim134@gmail.com>
2019-01-14 14:24:13 -0500

    Show an alert for the x and y button clicked

--- TicTacToe/TicTacToe.ReactWebApp/ClientApp/src/store/TicTacToe.js
+++ TicTacToe/TicTacToe.ReactWebApp/ClientApp/src/store/TicTacToe.js
@@ -1,22 +1,27 @@
 ﻿const initialState = { p1Btn_Value: '', p2Btn_Value: '', p3Btn_Value: '', p4Btn_Value: '', p5Btn_Value: '', p6Btn_Value: '', p7Btn_Value: '', p8Btn_Value: '', p9Btn_Value: '' };
 
 export const actionCreators = {
-    p1Btn_Click: () => ({ type: 'test' }),
-    p2Btn_Click: () => ({}),
-    p3Btn_Click: () => ({}),
-    p4Btn_Click: () => ({}),
-    p5Btn_Click: () => ({}),
-    p6Btn_Click: () => ({}),
-    p7Btn_Click: () => ({}),
-    p8Btn_Click: () => ({}),
-    p9Btn_Click: () => ({})
+    p1Btn_Click: () => ({ type: 'move', x: 0, y: 0 }),
+    p2Btn_Click: () => ({ type: 'move', x: 1, y: 0 }),
+    p3Btn_Click: () => ({ type: 'move', x: 2, y: 0 }),
+    p4Btn_Click: () => ({ type: 'move', x: 0, y: 1 }),
+    p5Btn_Click: () => ({ type: 'move', x: 1, y: 1 }),
+    p6Btn_Click: () => ({ type: 'move', x: 2, y: 1 }),
+    p7Btn_Click: () => ({ type: 'move', x: 0, y: 2 }),
+    p8Btn_Click: () => ({ type: 'move', x: 1, y: 2 }),
+    p9Btn_Click: () => ({ type: 'move', x: 2, y: 2 })
 };
+
+function makeMove(x, y) {
+    alert('button clicked: ' + x + ', ' + y);
+}
 
 export const reducer = (state, action) => {
   state = state || initialState;
 
-  if (action.type == 'test') {
-    return { ...state, p1Btn_Value: 'test' };
+    if (action.type == 'move') {
+        makeMove(action.x, action.y);
+    return { ...state };
   }
 
   return state;
