9190247ac9b36079438d66cda0138c923d05c590
Miguel Molina Jr <molim134@gmail.com>
2021-11-08 12:30:05 -0500

    upgrade to .net 5

--- TicTacToe/TicTacToe.ReactWebApp/Startup.cs
+++ TicTacToe/TicTacToe.ReactWebApp/Startup.cs
@@ -2,10 +2,10 @@ namespace TicTacToe.ReactWebApp
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
 
     public class Startup
     {
@@ -16,8 +16,7 @@ namespace TicTacToe.ReactWebApp
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -25,19 +24,19 @@ namespace TicTacToe.ReactWebApp
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
+                app.UseExceptionHandler("/Error").UseHsts();
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
+
+            app.UseStaticFiles().UseSpaStaticFiles();
 
             app.UseSession();
 
-            app.UseMvc(routes => { routes.MapRoute(name: "default", template: "{controller}/{action=Index}/{id?}"); });
-
-            app.UseSpa(spa =>
+            app.UseRouting().UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller}/{action=Index}/{id?}");
+            }).UseSpa(spa =>
             {
                 spa.Options.SourcePath = "ClientApp";
 
@@ -48,12 +47,10 @@ namespace TicTacToe.ReactWebApp
             });
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllers().AddNewtonsoftJson();
 
-            // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
 
             services.AddDistributedMemoryCache();
